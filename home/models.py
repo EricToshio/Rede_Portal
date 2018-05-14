f0beeddfd44ff0a05d1a8724ec01c7fbdb7c0238
EricToshio <eric_toshio@hotmail.com>
2018-05-14 18:25:12 -0300

    add image to model

--- home/models.py
+++ home/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
+import os
+
+def get_image_path(instance, filename):
+        return os.path.join('static', 'home', 'images', 'news',str(instance.id), filename)
 
 # Create your models here.
 class News(models.Model):
-    news_text = models.TextField()
-    pub_date = models.DateTimeField('date published')
+    
+
+    title = models.TextField(validators=[MaxLengthValidator(30)], default = 'Title')
+    sub_title = models.TextField(validators=[MaxLengthValidator(50)], default = 'Sub-titulo')
     iniciativa_name = models.CharField(max_length=100)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    pic = models.ImageField(upload_to = get_image_path, default = 'home/static/home/images/casd.png')
     # def __str__(self):
     #     return self.question_text
+    
 
 
 # class Choice(models.Model):
