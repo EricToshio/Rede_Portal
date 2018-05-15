37a6c5a85d7a2eeb96850aa25fb892c5195b2552
EricToshio <eric_toshio@hotmail.com>
2018-05-14 21:36:22 -0300

    add reported problems model

--- issues_report/models.py
+++ issues_report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Problem(models.Model):
+    name = models.CharField(max_length=50)
+    localization = models.CharField(max_length=5)
+    inciativa = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    ticket = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    status = models.CharField(max_length=20)
--- issues_report/views.py
+++ issues_report/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from .models import Problem
+
 def index(request):
-    context = {}
+    context = {'problems_list': Problem.objects.order_by('-pub_date')}
     return render(request, 'issues_report/index.html', context)
