239abe40b12eaffa7215fca7a5040df26dfaabb6
Duhamb <eduambarbosa@gmail.com>
2018-05-14 21:58:58 -0300

    Adding news-feed/

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from el_pagination.views import AjaxListView
 
 from .models import News
 
@@ -11,3 +12,14 @@ def index(request):
     context = {'news_list': News.objects.order_by('-pub_date')[1:4],
     		   'main_news': News.objects.order_by('-pub_date')[0]}
     return render(request, 'home/index.html', context)
+
+def news_feed(request,
+    template='home/news-feed-list.html',
+    page_template='home/news-feed-page.html'):
+    context = {
+        'news_list': News.objects.order_by('-pub_date'),
+        'page_template': page_template,
+    }
+    if request.is_ajax():
+        template = page_template
+    return render(request, template, context)
