f01f89ce85124528a52b3d7bfd9753c1632611c8
Claudio <claudiosilva.cfls@gmail.com>
2018-05-18 18:48:23 -0300

    New layout with carousel [incomplete]

--- Rede_Portal/settings.py
+++ Rede_Portal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
+    'bootstrap4',
     'home.apps.HomeConfig',
     'issues_report.apps.IssuesReportConfig',
     'django.contrib.admin',
@@ -123,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 
 # Endless-pagination settings
 
