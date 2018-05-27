7c716ca23372449eb97aa0bdb5be52d5e342b5ce
Claudio <claudiosilva.cfls@gmail.com>
2018-05-27 13:36:10 -0300

    Show problem report in redecasd page

--- redecasd/views.py
+++ redecasd/views.py
@@ -5,11 +5,17 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from issues_report.models import Problem
+
+def member(request):
+    context = {'problem_list': Problem.objects.filter(iniciativa="RedeCasd").order_by('-pub_date')[0:3]}
+    return render(request, 'redecasd/dashboard/index.html', context)
+
 
 def index(request):
     context = {}
     return render(request, 'redecasd/index.html', context)
 
-def member(request):
-    context = {}
-    return render(request, 'redecasd/dashboard/index.html', context)
+# def member(request):
+#     context = {}
+#     return render(request, 'redecasd/dashboard/index.html', context)
