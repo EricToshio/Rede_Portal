b6d2bb1518994c4f0a8b0d0babc2a6f952d71591
EricToshio <eric_toshio@hotmail.com>
2018-05-27 02:24:41 -0300

    issue report form choice in iniciativa

--- issues_report/forms.py
+++ issues_report/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class IssueForm(forms.Form):
     name = forms.CharField(label='Seu nome', initial='',max_length=50)
     localization = forms.CharField(label='AP/vaga (ex.: 315/F)', initial='', max_length=5)
-    iniciativa = forms.CharField(label='iniciativa (ex.: RedeCasd)', initial='',max_length=20)
+    iniciativa = forms.ChoiceField(label='iniciativa (ex.: RedeCasd)', choices= (('1','RedeCasd'),('2','DOO'),('3','Casd')))
     description = forms.CharField(label='Descrição do problema', initial='',max_length=200)
