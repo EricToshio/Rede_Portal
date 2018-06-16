65dde5013147631351725fbf4a5cce2736d39742
EricToshio <eric_toshio@hotmail.com>
2018-06-16 16:19:53 -0300

    login required to access redecasd information

--- redecasd/views.py
+++ redecasd/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
 import simplejson
 
 from issues_report.models import Problem
@@ -21,10 +23,12 @@ def schedule(request):
     context = {}
     return render(request, 'redecasd/reservas.html', context)
 
+@login_required
 def member(request):
     context = {'problem_list': Problem.objects.filter(iniciativa="RedeCasd").order_by('-pub_date')}
     return render(request, 'redecasd/dashboard/index.html', context)
-
+    
+@login_required
 def membros_reserva(request):
     context = {'pedidos_de_reserva': ReservationRede.objects.all()}
     return render(request, 'redecasd/dashboard/reserva-sala.html', context)
