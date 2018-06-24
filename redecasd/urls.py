1e7733d52636f790cb35157a7d03638c033e9efb
Claudio <claudiosilva.cfls@gmail.com>
2018-06-24 15:00:00 -0300

    Users can be added or removed to/from redecasd group

--- redecasd/urls.py
+++ redecasd/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 app_name = 'redecasd'
@@ -7,6 +7,9 @@ urlpatterns = [
     path('membros/', views.member, name='member'),
     path('membros/reservas', views.membros_reserva, name='membros_reserva'),
     path('membros/gerenciar-membros', views.membros_manage, name='membros_manage'),
+    path('membros/gerenciar-membros/remover/<int:pk>', views.membros_manage_remove, name='membros_manage_remove'),
+    re_path(r'^membros/gerenciar-membros/pesquisar-usuario/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', views.membros_manage_search, name='membros_manage_search'),
+    path('membros/gerenciar-membros/adicionar/<int:pk>', views.membros_manage_add, name='membros_manage_add'),
     path('membros/reservas/<slug:modo>/<int:pk>', views.membros_reserva_positive, name='membros_reserva_positive'),
     path('reservas/', views.schedule, name='schedule'),
     path('membros/update/<int:pk>', views.problem_report_update, name='problem_report_update'),
--- redecasd/views.py
+++ redecasd/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 import simplejson
 
@@ -41,8 +41,35 @@ def membros_reserva(request):
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='RedeCasd').count() == 1, login_url='/')
 def membros_manage(request):
-    context = {'membros': User.objects.filter(groups__name="RedeCasd")}
+    context = {'membros': User.objects.filter(groups__name="RedeCasd"), 'todos_usuarios': User.objects}
     return render(request, 'redecasd/dashboard/membros-gerenciar.html', context)
+
+def membros_manage_remove(request, pk):
+    grupo_da_redecasd = Group.objects.get(name="RedeCasd")
+    grupo_da_redecasd.user_set.remove(pk)
+    grupo_da_redecasd.save()
+    context = {'membros': User.objects.filter(groups__name="RedeCasd")}
+    data ={'html_member_list': render_to_string('redecasd/includes/partial_member_manage.html', context, request)}
+    return JsonResponse(data)
+
+def membros_manage_search(request, email):
+    usuarios_encontrados = User.objects.filter(email=email)
+    found = len(usuarios_encontrados) != 0
+    context = {'resultados': usuarios_encontrados, 'found': found, 'close_search': False }
+    data ={'html_results': render_to_string('redecasd/includes/partial_member_manage_search_result.html', context, request)}
+    return JsonResponse(data)
+
+def membros_manage_add(request, pk):
+    grupo_da_redecasd = Group.objects.get(name="RedeCasd")
+    grupo_da_redecasd.user_set.add(pk)
+    grupo_da_redecasd.save()
+    context = {'membros': User.objects.filter(groups__name="RedeCasd")}
+    context2 = {'resultados': None, 'found': False, 'close_search': True }
+    data ={
+       'html_member_list': render_to_string('redecasd/includes/partial_member_manage.html', context, request), 
+       'html_results' : render_to_string('redecasd/includes/partial_member_manage_search_result.html', context2, request), 
+    }
+    return JsonResponse(data)
 
 def membros_reserva_positive(request, modo, pk):
     reservation = ReservationRede.objects.get(pk=pk)
@@ -54,6 +81,7 @@ def membros_reserva_positive(request, modo, pk):
     context = {'pedidos_de_reserva': ReservationRede.objects.all()}
     data ={'html_reservation_list': render_to_string('redecasd/includes/partial_reservations_request.html', context, request)}
     return JsonResponse(data)
+
 
 def problem_report_update(request, pk):
     problem = get_object_or_404(Problem, pk=pk)
@@ -100,11 +128,8 @@ def save_reservation_form(request, context, template_name):
         if form.is_valid():
             form.save()
             data['form_is_valid'] = True
-            print("form eh valido")
         else:
             data['form_is_valid'] = False
-            print("form nao eh valido")
-            print(form)
     # context = {'form': form}
     form.fields['reservation_date'].initial = context['date']
     # form.fields['reservation_date'].widget = forms.HiddenInput()
