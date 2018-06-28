231a3d03f03e050ee406ea1adf0fa17e32848a6a
Eduardo Alarcon Mady Barbosa <eduambarbosa@gmail.com>
2018-06-27 21:16:46 -0300

    Members can add news

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('news-feed/', views.news_feed, name='news_feed'),
     # path('blank/', views.index_real, name='index_real'),
     path('news/', views.news, name='news'),
-    path('news-manager/', views.news_manager, name='news_manager')
+    path('news-manager/', views.news_manager, name='news_manager'),
+    path('news-manager/create/', views.news_create, name='news_create'),
+    path('news-manager/update/<int:pk>', views.news_update, name='news_update'),
+    path('news-manager/remove/<int:pk>', views.news_remove, name='news_remove')
 ]
--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from el_pagination.views import AjaxListView
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from .models import News
+from .forms import NewsForm
 
 def index(request):
     context = {'news_list_1': News.objects.order_by('-pub_date')[1:3],
@@ -38,3 +39,58 @@ def news_manager(request):
         'news_list': News.objects.order_by('-pub_date')
     }
     return render(request, 'home/manager/news-manager.html', context)
+
+def news_create(request):
+    form = NewsForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()        
+    else:
+        form = NewsForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'home/manager/news-add.html', context)
+
+def news_remove(request, pk):
+    news = get_object_or_404(News, pk=pk)
+
+    if request.method == 'POST':
+        form = NewsForm(request.POST, instance=news)
+        news.delete()
+    else:
+        form = NewsForm(instance=news)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'home/manager/news-edit.html', context)
+
+def news_update(request, pk):
+    news = get_object_or_404(News, pk=pk)
+    if request.method == 'POST':
+        form = NewsForm(request.POST, instance=news)
+    else:
+        form = NewsForm(instance=news)
+
+    context = {
+        'form': form,
+    }
+    return save_news_form(request, 'home/manager/news-edit.html', context)
+
+def save_news_form(request, template_name, form):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            problems = Problem.objects.filter(iniciativa="RedeCasd").order_by('-pub_date')
+            data['html_problem_list'] = render_to_string('home/manager/news_list.html', {
+                'problem_list': problems
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request)
+    return JsonResponse(data)
