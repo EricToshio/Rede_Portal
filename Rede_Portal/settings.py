c01f978e50cafed58519ca02d0aa8f6b776d8b75
EricToshio <eric_toshio@hotmail.com>
2018-06-28 00:08:32 -0300

    Deploy heroku changes again 4

--- Rede_Portal/settings.py
+++ Rede_Portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'uv=%^v&g^h6ex!du%qrb4d#m5^r3h8cn5rop^q_i4igyi(+03-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","portal-h8.herokuapp.com"]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -91,11 +92,14 @@ WSGI_APPLICATION = 'Rede_Portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rede_casd',
+        'USER': 'membro_rede',
+        'PASSWORD': 'azsxdc',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -151,4 +155,7 @@ EL_PAGINATION_PER_PAGE = 5
 
 LOGIN_REDIRECT_URL = '/'
 
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
